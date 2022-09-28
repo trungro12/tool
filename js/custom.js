a1ffb22b34eb28b120a149313b65cce0b7b7731d
Trung Pham <trungnhot12@gmail.com>
2022-09-28 22:44:13 +0700

    up

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// load content with jquery
+function loadContent(element, url) {
+  element
+    .html(
+      "<img alt='loading' style='max-width:70px' src='https://i.imgur.com/BsLJBhG.gif' />"
+    )
+    .load(url);
+}
