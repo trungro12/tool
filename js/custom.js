11d349101d65330cfc615e4f3ebfd2b127e180f5
Trung Pham <trungnhot12@gmail.com>
2022-09-30 00:15:38 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
+Number.prototype.formatBytes = function () {
+  var units = ["B", "KB", "MB", "GB", "TB"],
+    bytes = this,
+    i;
+
+  for (i = 0; bytes >= 1024 && i < 4; i++) {
+    bytes /= 1024;
+  }
+
+  return bytes.toFixed(2) + units[i];
+};
 // load content with jquery
 function loadContent(element, url) {
   element
@@ -9,7 +20,9 @@ function loadContent(element, url) {
 // base64image
 function readFile() {
   if (!this.files || !this.files[0]) return;
-
+  var name = this.files[0].name;
+  var size = this.files[0].size;
+  var type = this.files[0].type;
   const FR = new FileReader();
 
   FR.addEventListener("load", function (evt) {
@@ -17,14 +30,18 @@ function readFile() {
     document.querySelector("#b64").style.display = "block";
     document.querySelector("#b64").textContent = evt.target.result;
     document.querySelector("#result").innerHTML = "<h3>Result</h3>";
+    document.querySelector("#result").innerHTML += "<p>Name : " + name + "</p>";
+    document.querySelector("#result").innerHTML += "<p>Size : " + size.formatBytes() + "</p>";
+    document.querySelector("#result").innerHTML += "<p>Type : " + type + "</p>";
     document.querySelector("#preview").innerHTML = "<h3>Preview</h3>";
   });
 
   FR.readAsDataURL(this.files[0]);
 }
 
-// md5 
-function md5Generator(){
+// md5
+function md5Generator() {
   var result = md5($(this).val());
-  document.querySelector("#result").innerHTML = "<h3>Result</h3><p id='content-result'>" + result + "</p>";
+  document.querySelector("#result").innerHTML =
+    "<h3>Result</h3><p id='content-result'>" + result + "</p>";
 }
