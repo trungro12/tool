0306b882f46d766cc7d2540604d2875494aaf1a0
Trung Pham <trungnhot12@gmail.com>
2022-09-29 16:32:50 +0700

    add base64 converter

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,17 @@ function loadContent(element, url) {
     )
     .load(url);
 }
+function readFile() {
+  if (!this.files || !this.files[0]) return;
+
+  const FR = new FileReader();
+
+  FR.addEventListener("load", function (evt) {
+    document.querySelector("#img").src = evt.target.result;
+    document.querySelector("#b64").style.display = "block";
+    document.querySelector("#b64").textContent = evt.target.result;
+  });
+
+  FR.readAsDataURL(this.files[0]);
+}
+document.querySelector("#inp").addEventListener("change", readFile);
